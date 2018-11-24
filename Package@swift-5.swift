31cae648123b476f148328400e56064fcf14f7e8
Ron Pinz <ron.pinz@gmail.com>
2018-11-23 19:48:03 -0500

    add manifest for swift 5 update 4.2 manifest update CCogl submodule

--- Package@swift-5.swift
+++ Package@swift-5.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 
 import PackageDescription
 
@@ -32,7 +32,6 @@ let package = Package(
             ])
     ],
     swiftLanguageVersions: [
-        .v4_2,
-        .version("5")
+        .v5
     ]
 )
